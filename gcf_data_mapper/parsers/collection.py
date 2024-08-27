2ea94f4a4ddd9f51d8d6a70c7910fbeffe09cb43
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-08-27 15:53:56 +0100

    Move entity parser placeholders into own files

---
+++ gcf_data_mapper/parsers/collection.py
@@ -0,0 +1,20 @@
+from typing import Any, Optional
+
+import click
+
+
+def collection(debug: bool) -> list[Optional[dict[str, Any]]]:
+    """Map the GCF collection info to new structure.
+
+    Collection information is not currently available for GCF data, so
+    we will leave this function as not implemented for now.
+
+    :param bool debug: Whether debug mode is on.
+    :return list[Optional[dict[str, Any]]]: A list of GCF families in
+        the 'destination' format described in the GCF Data Mapper Google
+        Sheet.
+    """
+    if debug:
+        click.echo("📝 Wrangling GCF collection data.")
+
+    return []
