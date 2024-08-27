7ecc2608005d6e0ba914f276f81130f0ff1d8de1
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-08-27 14:24:14 +0100

    refactor: update cli.py file
    - add entry point for commands
    - remove test commands

--- gcf_data_mapper/cli.py
+++ gcf_data_mapper/cli.py
@@ -1,13 +1,23 @@
 import click
 
 
-@click.command()
-@click.option("--name", required=True, help="The name to greet.")
-@click.version_option("0.1.0", "--version", "-v", help="Show the version and exit.")
-def greet(name):
-    """Simple program that greets NAME."""
-    click.echo(f"Hello {name}!")
+@click.group()
+def cli():
+    pass
+
+
+@click.command("read_data_file")
+@click.argument("file_path", type=click.Path(exists=True))
+def read_data_file(file_path: str):
+    """Read the data file"""
+    click.echo("Read Data File")
+
+
+def entrypoint():
+    # trunk-ignore(pyright/reportFunctionMemberAccess)
+    cli.add_command(read_data_file)
+    cli()
 
 
 if __name__ == "__main__":
-    greet()
+    entrypoint()
