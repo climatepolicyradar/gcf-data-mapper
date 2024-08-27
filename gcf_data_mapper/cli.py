a9e132561992f0b71fecffa151b822727d0f5198
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-08-27 14:08:14 +0100

    Add function for wrangling collection data

--- gcf_data_mapper/cli.py
+++ gcf_data_mapper/cli.py
@@ -1,13 +1,31 @@
+import sys
+
 import click
 
 
 @click.command()
-@click.option("--name", required=True, help="The name to greet.")
 @click.version_option("0.1.0", "--version", "-v", help="Show the version and exit.")
-def greet(name):
-    """Simple program that greets NAME."""
-    click.echo(f"Hello {name}!")
+def wrangle_json():
+    """Simple program that wrangles GCF data into bulk import format."""
+    click.echo("🚀 Starting the GCF data mapping process.")
+
+    try:
+        collection()
+    except Exception as e:
+        click.echo(f"❌ Failed to map GCF data to expected JSON. Error: {e}.")
+        sys.exit(1)
+
+    click.echo("✅ Finished mapping GCF data.")
+
+
+def collection():
+    """Map the GCF collection info to new structure.
+
+    Collection information is not currently available for GCF data, so
+    we will leave this function as not implemented for now.
+    """
+    pass
 
 
 if __name__ == "__main__":
-    greet()
+    wrangle_json()
