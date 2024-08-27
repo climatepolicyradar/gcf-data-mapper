11627cc6133a2a3e58aaf6de2bc618e4052b3702
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-08-27 15:55:49 +0100

    fix: update cli for functions moved in merge

--- gcf_data_mapper/cli.py
+++ gcf_data_mapper/cli.py
@@ -1,12 +1,49 @@
 import csv
-
 import sys
 
 import click
 
 
-@click.command()
+@click.group()
 @click.version_option("0.1.0", "--version", "-v", help="Show the version and exit.")
+def cli():
+    pass
+
+
+def read_csv(file_path: str):
+    """Reads a csv file, we will just read the file for now and echo a value"""
+    with open(file_path, "r") as file:
+        csv_reader = csv.DictReader(file)
+        data = []
+        for line in csv_reader:
+            data.append(line["country"])
+        click.echo(data)
+
+
+def read_json(file_path: str):
+    """Reads a json file, we will just read the file for now and echo a value"""
+    with open(file_path, "r") as file:
+        data = file.read()
+        click.echo(data)
+
+
+@click.command("read_data_file")
+@click.argument("file_path", type=click.Path(exists=True))
+def read_data_file(file_path: str):
+    """Simple program that reads a data file, calls a function to read a csv or json file respectively"""
+    file_extension = file_path.lower().split(".")[-1]
+    try:
+        if file_extension not in ["json", "csv"]:
+            raise ValueError("File must be a valid json or csv file")
+        if file_extension == "csv":
+            read_csv(file_path)
+        elif file_extension == "json":
+            read_json(file_path)
+    except Exception as e:
+        click.echo(f"Error reading file: {e}")
+
+
+@click.command()
 def wrangle_json():
     """Simple program that wrangles GCF data into bulk import format."""
     click.echo("🚀 Starting the GCF data mapping process.")
@@ -29,5 +66,7 @@ def collection():
     pass
 
 
-if __name__ == "__main__":
-    wrangle_json()
+def entrypoint():
+    cli.add_command(read_data_file)
+    cli.add_command(wrangle_json)
+    cli()
--- tests/unit_tests/test_cli.py
+++ tests/unit_tests/test_cli.py
@@ -1,8 +1,31 @@
 import json
 
+import pytest
 from click.testing import CliRunner
 
-from gcf_data_mapper.cli import read_data_file
+from gcf_data_mapper.cli import read_data_file, wrangle_json
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(wrangle_json, ["--version"])
+    assert result.exit_code == 0
+    assert "version" in result.output.strip()
+
+
+@pytest.mark.skip()
+def test_wrangle_json_fail():
+    runner = CliRunner()
+    result = runner.invoke(wrangle_json)
+    assert result.exit_code == 1
+    assert "Failed to map GCF data to expected JSON" in result.output.strip()
+
+
+def test_wrangle_json_success():
+    runner = CliRunner()
+    result = runner.invoke(wrangle_json)
+    assert result.exit_code == 0
+    assert "Finished mapping GCF data." in result.output.strip()
 
 
 def get_json_test_data():
@@ -34,29 +57,3 @@ def test_errors_on_invalid_file():
         "Error reading file: File must be a valid json or csv file"
         in result.output.strip()
     )
-import pytest
-from click.testing import CliRunner
-
-from gcf_data_mapper.cli import wrangle_json
-
-
-def test_version():
-    runner = CliRunner()
-    result = runner.invoke(wrangle_json, ["--version"])
-    assert result.exit_code == 0
-    assert "version" in result.output.strip()
-
-
-@pytest.mark.skip()
-def test_wrangle_json_fail():
-    runner = CliRunner()
-    result = runner.invoke(wrangle_json)
-    assert result.exit_code == 1
-    assert "Failed to map GCF data to expected JSON" in result.output.strip()
-
-
-def test_wrangle_json_success():
-    runner = CliRunner()
-    result = runner.invoke(wrangle_json)
-    assert result.exit_code == 0
-    assert "Finished mapping GCF data." in result.output.strip()
