1c64590d75089fed5aeb7f043b8ce8b380b7c99d
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-08-27 18:12:45 +0100

    test: refactor tests to use parametrize

--- tests/unit_tests/test_read_data.py
+++ tests/unit_tests/test_read_data.py
@@ -6,27 +6,35 @@ from gcf_data_mapper.read import read_data_file
 
 
 @pytest.fixture()
-def get_json_test_data():
-    """Expected Json Data Structure"""
-    assert os.path.exists("tests/unit_tests/test_fixtures/test.json")
-    yield read_data_file("tests/unit_tests/test_fixtures/test.json")
+def get_test_data(request):
+    """Fixture to yield expected data structure based on file type."""
+    file_path = request.param
+    assert os.path.exists(file_path)
+    yield read_data_file(file_path)
 
 
-@pytest.fixture()
-def get_csv_test_data():
-    """Expected CSV Data Structure"""
-    assert os.path.exists("tests/unit_tests/test_fixtures/test.csv")
-    yield read_data_file("tests/unit_tests/test_fixtures/test.csv")
-
-
-def test_reads_json_files(get_json_test_data):
+@pytest.mark.parametrize(
+    "get_test_data",
+    [
+        "tests/unit_tests/test_fixtures/test.json",
+    ],
+    indirect=True,
+)
+def test_reads_json_files(get_test_data):
     data = read_data_file("tests/unit_tests/test_fixtures/test.json")
-    assert data == get_json_test_data
+    assert data == get_test_data
 
 
-def test_reads_csv_files(get_csv_test_data):
+@pytest.mark.parametrize(
+    "get_test_data",
+    [
+        "tests/unit_tests/test_fixtures/test.csv",
+    ],
+    indirect=True,
+)
+def test_reads_csv_files(get_test_data):
     data = read_data_file("tests/unit_tests/test_fixtures/test.csv")
-    assert data == get_csv_test_data
+    assert data == get_test_data
 
 
 def test_errors_on_invalid_file():
