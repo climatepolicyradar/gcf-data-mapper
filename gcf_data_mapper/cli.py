633289b8cf36c2226c7a49648e11487a87536dd6
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-08-27 15:54:40 +0100

    Use functions from parsers and add debug mode

--- gcf_data_mapper/cli.py
+++ gcf_data_mapper/cli.py
@@ -1,31 +1,58 @@
 import sys
+from typing import Any, Optional
 
 import click
 
+from gcf_data_mapper.parsers.collection import collection
+from gcf_data_mapper.parsers.family import family
+
 
 @click.command()
+@click.option("--debug/--no-debug", default=False)
 @click.version_option("0.1.0", "--version", "-v", help="Show the version and exit.")
-def wrangle_json():
-    """Simple program that wrangles GCF data into bulk import format."""
+def entrypoint(debug: bool):
+    """Simple program that wrangles GCF data into bulk import format.
+
+    :param bool debug: Whether debug mode is on.
+    """
     click.echo("🚀 Starting the GCF data mapping process.")
 
     try:
-        collection()
+        wrangle_to_json(debug)
     except Exception as e:
         click.echo(f"❌ Failed to map GCF data to expected JSON. Error: {e}.")
         sys.exit(1)
 
     click.echo("✅ Finished mapping GCF data.")
 
+    click.echo()
+    click.echo("🚀 Dumping GCF data to output file")
+    dump_output()
+    click.echo("✅ Finished dumping mapped GCF data.")
 
-def collection():
-    """Map the GCF collection info to new structure.
 
-    Collection information is not currently available for GCF data, so
-    we will leave this function as not implemented for now.
+def wrangle_to_json(debug) -> dict[str, list[Optional[dict[str, Any]]]]:
+    """Put the mapped GCF data into a dictionary ready for dumping.
+
+    The output of this function will get dumped as JSON to the output
+    file.
+
+    :param bool debug: Whether debug mode is on.
+    :return dict[str, list[Optional[dict[str, Any]]]]: The GCF data
+        mapped to the Document-Family-Collection-Event entity it
+        corresponds to.
     """
+    return {
+        "collections": collection(debug),
+        "families": family(debug),
+        "documents": [],
+        "events": [],
+    }
+
+
+def dump_output():
     pass
 
 
 if __name__ == "__main__":
-    wrangle_json()
+    entrypoint()
