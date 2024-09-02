cab563c429f542fd99197333830af71719a5c839
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-09-02 14:10:16 +0100

    Write tests for read()

--- tests/unit_tests/read/test_read.py
+++ tests/unit_tests/read/test_read.py
@@ -1,13 +1,10 @@
-import json
 import os
-from typing import Any, Union
 
+import pandas as pd
 import pytest
 
 from gcf_data_mapper.read import read
-
-UNIT_TESTS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-FIXTURES_FOLDER = os.path.join(UNIT_TESTS_FOLDER, "fixtures")
+from tests.unit_tests.read.conftest import FIXTURES_FOLDER
 
 
 def return_valid_csv_data():
@@ -69,73 +66,108 @@ def return_valid_json_data():
             "rivers": {"names": [{"egypt": "Nile"}, {"london": "Thames"}]},
             "natural_disasters": ["Blizzards", "Wildfires"],
         },
+        {
+            "country": "Egypt",
+            "capital": "Cairo",
+            "climate_info": {
+                "avg_temp_celsius": 26,
+                "annual_rainfall_mm": 80,
+                "climate_zone": "Desert",
+            },
+            "rivers": {"names": [{"egypt": "Nile"}, {"london": "Thames"}]},
+            "natural_disasters": ["Droughts"],
+        },
     ]
     return json_data
 
 
-@pytest.mark.parametrize(
-    "filepath, expected_output",
-    (
-        (
-            os.path.join(FIXTURES_FOLDER, "valid_climate_json_data.json"),
-            return_valid_json_data(),
-        ),
-        (
-            os.path.join(FIXTURES_FOLDER, "valid_climate_csv_data.csv"),
-            return_valid_csv_data(),
-        ),
-    ),
-)
-def test_valid_files_return_expected_output(
-    filepath: str, expected_output: Union[dict, list[dict[str, Any]]]
-):
-    assert os.path.exists(filepath)
-    data = read(filepath)
-    assert data is not None
-    assert data == expected_output
+def test_valid_files_return_expected_output():
+    fam_data, doc_data = read(
+        os.path.join(FIXTURES_FOLDER, "valid_climate_csv_data.csv"),
+        os.path.join(FIXTURES_FOLDER, "valid_climate_json_data.json"),
+        os.path.join(FIXTURES_FOLDER, "valid_climate_csv_data_2_records.csv"),
+    )
+
+    assert fam_data is not None
+    assert (
+        pd.testing.assert_frame_equal(
+            fam_data,
+            pd.DataFrame(
+                {
+                    "country": ["Brazil", "Canada", "Egypt"],
+                    "capital": ["Brasilia", "Ottawa", "Cairo"],
+                    "climate_info.avg_temp_celsius": [21.5, 6.3, 22.1],
+                    "climate_info.annual_rainfall_mm": [1500, 940, 25],
+                    "climate_info.climate_zone": ["Tropical", "Continental", "Desert"],
+                    "rivers.names": [
+                        [{"egypt": "Nile"}, {"london": "Thames"}],
+                        [{"egypt": "Nile"}, {"london": "Thames"}],
+                        [{"egypt": "Nile"}, {"london": "Thames"}],
+                    ],
+                    "natural_disasters": [
+                        ["Floods", "Landslides"],
+                        ["Blizzards", "Wildfires"],
+                        ["Droughts"],
+                    ],
+                }
+            ),
+            check_like=True,  # Ignore the order of columns and records.
+        )
+        is None
+    )
+
+    assert doc_data is not None
+    assert (
+        pd.testing.assert_frame_equal(
+            doc_data,
+            pd.DataFrame(
+                {"country": ["Brazil", "Canada"], "capital": ["Brasilia", "Ottawa"]}
+            ),
+        )
+        is None
+    )
 
 
 @pytest.mark.parametrize(
-    "filepath, expected_output",
-    (
+    "filepath,error,error_msg",
+    [
         (
-            os.path.join(FIXTURES_FOLDER, "invalid_climate_json_data.json"),
-            return_valid_json_data(),
+            os.path.join(FIXTURES_FOLDER, "empty_file.csv"),
+            ValueError,
+            "One or more of the expected dataframes are empty",
+        ),
+        (
+            os.path.join(FIXTURES_FOLDER, "malformed_data.json"),
+            ValueError,
+            "One or more of the expected dataframes are empty",
         ),
         (
             os.path.join(FIXTURES_FOLDER, "invalid_climate_csv_data.csv"),
-            return_valid_csv_data(),
+            ValueError,
+            "One or more of the expected dataframes are empty",
         ),
-    ),
+        (
+            os.path.join(FIXTURES_FOLDER, "test_text_file.txt"),
+            ValueError,
+            "Error reading file: File must be a valid json or csv file",
+        ),
+        (
+            os.path.join(FIXTURES_FOLDER, "non_existent_file.csv"),
+            FileNotFoundError,
+            "No such file or directory",
+        ),
+        (
+            os.path.join(FIXTURES_FOLDER, "valid_climate_csv_data_2_records.csv"),
+            ValueError,
+            "Record number mismatch",
+        ),
+    ],
 )
-def test_invalid_files_do_not_return_expected_output(
-    filepath: str, expected_output: Union[dict, list[dict[str, Any]]]
-):
-    assert os.path.exists(filepath)
-    data = read(filepath)
-    assert data != expected_output
-
-
-def test_raises_error_on_invalid_file_extension():
-    with pytest.raises(ValueError) as e:
-        read(os.path.join(FIXTURES_FOLDER, "test_text_file.txt"))
-    assert str(e.value) == ("Error reading file: File must be a valid json or csv file")
-
-
-def test_raises_error_with_non_existent_file():
-    non_existent_file_path = os.path.join(FIXTURES_FOLDER, "non_existent_file.csv")
-    with pytest.raises(FileNotFoundError) as e:
-        read(non_existent_file_path)
-    assert str(e.value) == f"No such file or directory: '{non_existent_file_path}'"
-
-
-def test_raises_error_with_empty_file():
-    empty_file_path = os.path.join(FIXTURES_FOLDER, "empty_file.csv")
-    with pytest.raises(ValueError) as e:
-        read(empty_file_path)
-    assert str(e.value) == "Error reading file: File is empty"
-
-
-def test_raises_error_on_malformed_json():
-    with pytest.raises(json.JSONDecodeError):
-        read(os.path.join(FIXTURES_FOLDER, "malformed_data.json"))
+def test_raises(filepath, error, error_msg):
+    with pytest.raises(error) as e:
+        read(
+            os.path.join(FIXTURES_FOLDER, "valid_climate_json_data.json"),
+            filepath,
+            os.path.join(FIXTURES_FOLDER, "valid_climate_csv_data.csv"),
+        )
+    assert error_msg in str(e.value)
