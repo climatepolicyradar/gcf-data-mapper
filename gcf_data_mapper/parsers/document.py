5d60c169589477f27e81e5f895c3dc6a181a8d44
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-09-02 15:00:50 +0100

    WIP : feat - update documents function to map  data from csv

--- gcf_data_mapper/parsers/document.py
+++ gcf_data_mapper/parsers/document.py
@@ -1,7 +1,40 @@
+from enum import Enum
 from typing import Any, Optional
 
 import click
 import pandas as pd
+
+
+class DocumentColumns(Enum):
+    TRANSLATED_FILES = "Translated files"
+    TYPE = "Type"
+    TITLE = "Title"
+    SOURCE_URL = "Document page permalink"
+
+
+def map_translated_files(translated_files_row: pd.Series) -> list[dict]:
+    """
+    Maps the GCF document with translated versions into the new json structure
+
+    :param pd.Series translated_files_row: A row from the DataFrame containing the 'Translated files' field, which holds a string of translated source URLs separated by the pipe (|) character. This string includes multiple URLs for translated documents in various languages.
+    :return: A list of mcf document objects, each with a different source url reflecting the translated version of the original document
+    """
+
+    mapped_documents = []
+
+    files_string = str(translated_files_row["Translated files"])
+    list_of_translated_doc_urls = files_string.split("|")
+
+    for url in list_of_translated_doc_urls:
+        mapped_documents.append(
+            {
+                "metadata": {"type": translated_files_row[DocumentColumns.TYPE.value]},
+                "title": translated_files_row[DocumentColumns.TITLE.value],
+                "source_url": url,
+                "variant_name": "Translated",
+            }
+        )
+    return mapped_documents
 
 
 def document(mcf_docs: pd.DataFrame, debug: bool) -> list[Optional[dict[str, Any]]]:
@@ -16,4 +49,20 @@ def document(mcf_docs: pd.DataFrame, debug: bool) -> list[Optional[dict[str, Any
     if debug:
         click.echo("📝 Wrangling GCF document data.")
 
-    return []
+    mapped_docs = []
+    # trunk-ignore(cspell/error)
+    for _, row in mcf_docs.iterrows():
+        # trunk-ignore(cspell/error)
+        has_translated_files = pd.notna(row.at["Translated files"])
+        mapped_docs.append(
+            {
+                "metadata": {"type": row[DocumentColumns.TYPE.value]},
+                "title": row[DocumentColumns.TITLE.value],
+                "source_url": row[DocumentColumns.SOURCE_URL.value],
+                "variant_name": "Original Translation",
+            }
+        )
+        if has_translated_files:
+            mapped_docs.extend(map_translated_files(row))
+
+    return mapped_docs
