56c05f203340871c731a60b52c0f865ee9b0bd45
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-09-05 07:31:11 +0100

    feat: add helper functions
    checks if row has any required columns with empty values, checks if required columns from a row are missing

--- gcf_data_mapper/parsers/helpers.py
+++ gcf_data_mapper/parsers/helpers.py
@@ -8,7 +8,6 @@ def verify_required_fields_present(
 
     :param pd.DataFrame data: The DataFrame to check.
     :param set[str] required_fields: The required DataFrame columns.
-    :param bool debug: Whether debug mode is on.
     :raise AttributeError if any of the required fields are missing.
     :return bool: True if the DataFrame contains the required fields.
     """
@@ -20,3 +19,41 @@ def verify_required_fields_present(
         f"Required fields '{str(diff)}' not present in df columns '"
         f"{cols if cols != set() else r'{}'}'"
     )
+
+
+def check_row_for_columns_with_empty_values(
+    row: pd.Series, required_columns: list[str]
+):
+    """
+    Check that all required values in the given row are not empty (notna).
+
+    :param pd.Series row: The row to check for notna values.
+    :param list[str] required_columns: A list of column names that will be used to verify.
+    :raises ValueError: If any value in the row is notna.
+    """
+
+    # Ensure we are working with a pandas Series by re-selecting the required columns as a Series
+    row_subset = pd.Series(row[required_columns], index=required_columns)
+
+    breakpoint()
+    if row_subset.isna().any():
+        raise ValueError("This row has empty values")
+
+
+def check_row_for_missing_columns(row: pd.Series, required_columns: list[str]):
+    """
+    Check if a given row contains all required columns.
+
+    :param pd.Series row: The row (Series) to check for missing columns.
+    :param list[str] required_columns: A list of column names that will be used to verify.
+    :raises AttributeError: If any required columns are missing from the row.
+    """
+
+    missing_columns = set(required_columns).difference(set(row.index))
+
+    if missing_columns:
+        raise AttributeError(
+            "The data series is missing these required columns: {}".format(
+                ", ".join(missing_columns)
+            )
+        )
